e8755180623a2d45a6f57ff49a3c8157f85c4927
Simon Doty <simon@vast.com>
2016-05-10 00:05:51 +0200

    initial commit

---
+++ controllers/apiController.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var Event = require("../data/eventSchema");
+var router = require("express").Router();
+
+// attach our single get route for now
+router.route("/shows/:id?").get(getEvents);
+
+function getEvents(req, res) {
+    // var params = req.params.id ? {id: req.params.id} : {};
+    Event.find({}, function (err, events) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.json(events);
+        }
+    });
+}
+
+// function deleteShow(req, res) {
+//     var id = req.params.id;
+//     Show.remove({ _id: id }, function (err, removed) {
+//         if (err)
+//             res.send(err)
+//         else
+//             res.json(removed);
+//     });
+// }
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+
+//Express request pipeline
+var app = express();
+
+// attach middleware used by all routes
+app.use(bodyParser.json());
+
+// attach rinky-dinky routers
+// TODO: add versioning to config
+app.use("/api/v1", require("./controllers/apiController"));
+
+// start serving
+app.listen(8989, function () {
+    console.log("Started listening on port", 8989);
+});
+
+// Connect to mongodb database
+// TODO: configure this and change DB to events
+mongoose.connect("mongodb://localhost/shows");
